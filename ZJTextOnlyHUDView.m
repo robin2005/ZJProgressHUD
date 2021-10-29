87899400845d33735aa4081ba70ab6a1388b573e
强 <57048685@qq.com>
2021-10-29 13:56:04 +0800

    commit init
    commit init

---
+++ ZJTextOnlyHUDView.m
@@ -0,0 +1,91 @@
+//
+//  ZJTextOnlyHUDView.m
+//  ZJProgressHUD
+//
+//  Created by ZeroJ on 16/9/7.
+//  Copyright © 2016年 ZeroJ. All rights reserved.
+//
+
+#import "ZJTextOnlyHUDView.h"
+#import "ZJPrivateHUDProtocol.h"
+@interface ZJTextOnlyHUDView ()<ZJPrivateHUDProtocol>
+@property (strong, nonatomic) UILabel *label;
+
+@end
+
+@implementation ZJTextOnlyHUDView
+
+
+- (instancetype)init {
+    if (self = [super init]) {
+        [self commonInit];
+        
+    }
+    return self;
+}
+- (instancetype)initWithFrame:(CGRect)frame {
+    if (self = [super initWithFrame:frame]) {
+        [self commonInit];
+    }
+    return self;
+}
+
+
+- (void)commonInit {
+    [self addSubview:self.label];
+    self.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.60];
+    self.layer.cornerRadius = 14.0f;
+    self.layer.masksToBounds = YES;
+}
+
+- (void)dealloc {
+//    NSLog(@"text----dealloc");
+}
+
+- (void)layoutSubviews {
+    self.label.frame = self.bounds;
+}
+
+
+- (void)setText:(NSString *)text {
+    self.label.text = text;
+    const CGFloat widthpadding = 32.0f;
+    const CGFloat heightpadding = 6.0f;
+
+//    const CGFloat padding = 10.0f;
+
+    CGRect textRect = [text boundingRectWithSize:CGSizeMake(MAXFLOAT, 100.0f) options:NSStringDrawingUsesLineFragmentOrigin attributes:@{NSFontAttributeName: self.label.font} context:nil];
+    CGFloat screenWidth = [UIScreen mainScreen].bounds.size.width;
+    CGFloat screenHeight = [UIScreen mainScreen].bounds.size.height;
+
+    CGRect frame = self.frame;
+    frame.size.width = textRect.size.width + 2*widthpadding;
+    if (frame.size.width > screenWidth) {
+        frame.size.width = screenWidth - 2*widthpadding;
+    }
+    frame.origin.x = (screenWidth - frame.size.width)/2;
+    frame.size.height = textRect.size.height + 2*heightpadding;
+    frame.origin.y = (screenHeight - frame.size.height)/2;
+
+    self.frame = frame;
+    [self layoutIfNeeded];
+}
+
+- (void)setTextColor:(UIColor *)textColor {
+    self.label.textColor = textColor;
+}
+
+- (UILabel *)label {
+    if (!_label) {
+        UILabel *label = [[UILabel alloc] init];
+        label.textAlignment = NSTextAlignmentCenter;
+        label.textColor = [UIColor whiteColor];
+        label.numberOfLines = 0;
+        label.font = [UIFont systemFontOfSize:14.0];
+        _label = label;
+    }
+    
+    return _label;
+}
+
+@end
